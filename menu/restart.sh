19a721162cc8aa66077cf8a59df89853d50b8ced
Dexter Eskalarte <167977652+Mediatek-Script-Final@users.noreply.github.com>
2024-11-04 15:44:03 +0800

    Update restart.sh

--- menu/restart.sh
+++ menu/restart.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Restart All Services"
@@ -32,7 +32,7 @@ case $Restart in
                 1)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -109,7 +109,7 @@ case $Restart in
                 4)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -126,7 +126,7 @@ case $Restart in
                 5)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -143,7 +143,7 @@ case $Restart in
                 6)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -160,7 +160,7 @@ case $Restart in
                 7)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -177,7 +177,7 @@ case $Restart in
                 8)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -195,7 +195,7 @@ case $Restart in
                 9)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -214,7 +214,7 @@ case $Restart in
                 10)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
@@ -235,7 +235,7 @@ case $Restart in
                 11)
                 clear
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-                echo -e "\E[0;100;33m         • RESTART MENU •          \E[0m"
+                echo -e "\E[0;100;33m   • DEXTER RESTART MENU •   \E[0m"
                 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
                 echo -e ""
                 echo -e "[ \033[32mInfo\033[0m ] Restart Begin"
